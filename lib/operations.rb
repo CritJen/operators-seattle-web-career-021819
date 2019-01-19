0968653d08ce8c8d682c6d1c2b6ef5aa870dc4e6
Jennifer Clark <jen.boyd.clark@gmail.com>
2019-01-19 22:00:03 +0000

    Automatically backed up by Learn

--- lib/operations.rb
+++ lib/operations.rb
@@ -1,3 +1,4 @@
+require pry
 def unsafe?(speed)
   if speed > 60 || speed < 40
 		true
@@ -6,6 +7,7 @@ def unsafe?(speed)
 end
 
 def not_safe?(speed)
-  speed > 60 || speed < 40 ? true : false
+  binding.pry
+	speed > 60 || speed < 40 ? true : false
 end
 end
